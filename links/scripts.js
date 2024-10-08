237ff86a8f574f8174e99a326866ceab1ee25375
Maya <46725686+mayamiz@users.noreply.github.com>
2024-10-07 21:56:36 -0400

    Add files via upload

---
+++ links/scripts.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    var x = document.getElementById("archives");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
+
+  function myFunction1() {
+    var x = document.getElementById("graphics");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  } 
