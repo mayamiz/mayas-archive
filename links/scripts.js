dbb39f4595ff5e361af5df56f9e1d8c8abb11dd8
Maya <46725686+mayamiz@users.noreply.github.com>
2024-10-14 17:04:10 -0400

    Update scripts.js

--- links/scripts.js
+++ links/scripts.js
@@ -1,9 +1,6 @@
 function myFunction() {
     var x = document.getElementById("archives");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
+    x.classList.toggle(".container button"); 
     }
   } 
 
