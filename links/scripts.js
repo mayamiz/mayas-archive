bc863795bf5ba9d7b0b74ebc0d0f97c7d4de9825
Maya <46725686+mayamiz@users.noreply.github.com>
2024-10-14 20:00:51 -0400

    update class

--- links/scripts.js
+++ links/scripts.js
@@ -1,41 +1,43 @@
-function myFunction() {
-    var x = document.getElementById("archives");
-    x.classList.toggle(".container button"); 
-    }
-  } 
 
+  function myFunction() {
+    var x = document.getElementsByClassName("link")[0];
+    if (x.style.display === "none" || x.style.display === "") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
   function myFunction1() {
-    var x = document.getElementById("graphics");
-    if (x.style.display === "none") {
+    var x = document.getElementsByClassName("link")[1];
+    if (x.style.display === "none" || x.style.display === "") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-  } 
-
+  }
   function myFunction2() {
-    var x = document.getElementById("articles");
-    if (x.style.display === "none") {
+    var x = document.getElementsByClassName("link")[2];
+    if (x.style.display === "none" || x.style.display === "") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-  } 
-
+  }
   function myFunction3() {
-    var x = document.getElementById("inspo");
-    if (x.style.display === "none") {
+    var x = document.getElementsByClassName("link")[3];
+    if (x.style.display === "none" || x.style.display === "") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-  } 
-
+  }
   function myFunction4() {
-    var x = document.getElementById("useful");
-    if (x.style.display === "none") {
+    var x = document.getElementsByClassName("link")[4];
+    if (x.style.display === "none" || x.style.display === "") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-  } 
+  }
+
+
